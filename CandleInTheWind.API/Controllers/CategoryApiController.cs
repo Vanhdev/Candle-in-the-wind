852fbab22aa46be9d7a07d3a1eb34df07e6398db
phammanhhung <phammanhhung10501@gmail.com>
2021-11-29 23:46:52 +0700

    Add reference from CandleInTheWind.API to CandleInTheWind

---
+++ CandleInTheWind.API/Controllers/CategoryApiController.cs
@@ -0,0 +1,30 @@
+﻿using CandleInTheWind.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandleInTheWind.API.Controllers
+{
+    [ApiController]
+    [Route("api/category")]
+    public class CategoryApiController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public CategoryApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var categories = _context.Products.ToList();
+            return Ok(categories);
+        }
+    }
+}
