c7b39adcf48bf0fe15fe91767008fda8c97a8c8f
phammanhhung <phammanhhung10501@gmail.com>
2021-12-31 18:03:46 +0700

    Update content of edit comment

--- CandleInTheWind.API/Models/Comments/CommentCreateDTO.cs
+++ CandleInTheWind.API/Models/Comments/CommentCreateDTO.cs
@@ -1,7 +1,10 @@
-﻿namespace CandleInTheWind.API.Models.Comments
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CandleInTheWind.API.Models.Comments
 {
     public class CommentCreateDTO
     {
+        [Required]
         public string Content { get; set; }
     }
 }
